28c3c2904d0afd2ca24eb028ad20c66a705d8815
KookaS <olivier.charrez@gmail>
2023-09-08 23:19:21 +0000

    max 3 azs

--- module/aws/network/vpc/main.tf
+++ module/aws/network/vpc/main.tf
@@ -1,16 +1,17 @@
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
 locals {
   # https://registry.terraform.io/modules/terraform-aws-modules/vpc/aws/5.1.1?utm_content=documentLink&utm_medium=Visual+Studio+Code&utm_source=terraform-ls#private-versus-intra-subnets
   tier_tags = ["private", "public", "intra"]
 
   # increment from `0.0.0.0/16` to `0.0.16.0/16`
+  azs = try(slice(data.aws_availability_zones.available.names, 0, 3), data.aws_availability_zones.available.names)
   subnets = {
     for i, tier in local.tier_tags :
-    "${tier}" => [for az_idx in range(0, length(data.aws_availability_zones.available.names)) : cidrsubnet(var.cidr_ipv4, 4, i * length(data.aws_availability_zones.available.names) + az_idx)]
+    "${tier}" => [for az_idx in range(0, length(local.azs)) : cidrsubnet(var.cidr_ipv4, 4, i * length(local.azs) + az_idx)]
   }
-}
-
-data "aws_availability_zones" "available" {
-  state = "available"
 }
 
 module "vpc" {
@@ -24,7 +25,7 @@ module "vpc" {
   single_nat_gateway     = var.nat == "vpc" ? true : false
   one_nat_gateway_per_az = var.nat == "az" ? true : false
 
-  azs             = data.aws_availability_zones.available.names
+  azs             = local.azs
   public_subnets  = local.subnets["public"]
   private_subnets = local.subnets["private"]
   intra_subnets   = local.subnets["intra"]
